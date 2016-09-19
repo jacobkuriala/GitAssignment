fd20e5ee5a4ad75eb918c86fb685ccd2495f8b03
Jacob Kattampilly <jacobkuriala@gmail.com>
2016-09-18 23:20:43 -0400

    Test Axe In Game
    Testing the newly created axe class using a console application.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -12,16 +12,25 @@ package edu.fsu.cs.cen5035;
 public class RoleGame {
 
     public static void main(String[] args) {
+        //Testing sword
         Weapon sword = WeaponFactory.getWeapon("sword");
         System.out.println("Sword has " + sword.hit() + " of damage.");
         int armor = 20;
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        
-        //TODO: Add the remainig weapons here
+
+        //Testing arrow
         Weapon arrow = WeaponFactory.getWeapon("arrow");
         System.out.println("Arrow has " + arrow.hit() + " of damage.");
         armor = 20;
         System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+        //Testing Axe
+        Weapon axe = WeaponFactory.getWeapon("axe");
+        System.out.println("Axe has " + axe.hit() + " of damage.");
+        armor = 20;
+        System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        armor = 19;
+        System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
     }
 }
